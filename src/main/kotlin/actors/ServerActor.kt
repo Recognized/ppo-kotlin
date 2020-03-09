ec9c776f7f03f7cf971215228dd009408314de19
vladislav.saifulin <vladislav.saifulin@jetbrains.com>
2020-03-09 14:20:22 +0300

    Updates to task1

--- src/main/kotlin/actors/ServerActor.kt
+++ src/main/kotlin/actors/ServerActor.kt
@@ -6,7 +6,8 @@ import java.time.Duration
 import kotlin.coroutines.CoroutineContext
 
 data class SearchRequest(val query: String)
-data class SearchResult(val pages: List<String>)
+data class SearchResult(val provider: String, val pages: List<String>)
+data class TotalResult(val results: List<SearchResult>)
 
 class SearchSupervisor(private val searchers: Set<Class<SearchConcrete>>) : UntypedAbstractActor() {
     private val results = mutableListOf<SearchResult>()
@@ -27,9 +28,13 @@ class SearchSupervisor(private val searchers: Set<Class<SearchConcrete>>) : Unty
             }
             is SearchResult -> {
                 results += message
-                if (results.size == 3)
+                if (results.size == 3) {
+                    context.parent.tell(TotalResult(results.toList()), self)
+                    context.stop(self)
+                }
             }
             is ReceiveTimeout -> {
+                context.parent.tell(TotalResult(results.toList()), self)
                 context.stop(self)
             }
         }
@@ -45,7 +50,7 @@ abstract class SearchConcrete : UntypedAbstractActor() {
             is SearchRequest -> {
                 context.actorOf(Props.create(CoroutineActor::class.java) {
                     CoroutineActor {
-                        SearchResult(doSearch(message.query))
+                        SearchResult(this::class.java.simpleName, doSearch(message.query))
                     }
                 }).apply {
                     tell(Unit, sender)
@@ -54,6 +59,39 @@ abstract class SearchConcrete : UntypedAbstractActor() {
         }
     }
 }
+
+private fun fakeSearch(): List<String> {
+    return (1..10).map {
+        "$it"
+    }
+}
+
+open class GoogleSearch : SearchConcrete() {
+    override suspend fun doSearch(query: String): List<String> = fakeSearch()
+}
+
+open class YandexSearch : SearchConcrete() {
+    override suspend fun doSearch(query: String): List<String> = fakeSearch()
+}
+
+open class BingSearch : SearchConcrete() {
+    override suspend fun doSearch(query: String): List<String> = fakeSearch()
+}
+
+class SlowYandexSearch : YandexSearch() {
+    override suspend fun doSearch(query: String): List<String> {
+        delay(5000)
+        return super.doSearch(query)
+    }
+}
+
+class SlowBingSearch : BingSearch() {
+    override suspend fun doSearch(query: String): List<String> {
+        delay(5000)
+        return super.doSearch(query)
+    }
+}
+
 
 class CoroutineActor<T>(
     context: CoroutineContext = Dispatchers.IO,
@@ -81,9 +119,33 @@ class CoroutineActor<T>(
     }
 }
 
+class SearchPrinter(private val searchers: Set<Class<SearchConcrete>>) : UntypedAbstractActor() {
+    override fun onReceive(message: Any) {
+        when (message) {
+            is TotalResult -> {
+                println(message)
+            }
+            else -> {
+                context.actorOf(Props.create(SearchSupervisor::class.java, searchers)).tell(message, self)
+            }
+        }
+    }
+}
+
 fun main() {
     ActorSystem.create("MySystem").apply {
-        actorOf(Props.create(SearchSupervisor::class.java)).apply {
+        actorOf(
+            Props.create(
+                SearchPrinter::class.java,
+                setOf(
+                    GoogleSearch::class.java,
+                    YandexSearch::class.java,
+                    BingSearch::class.java,
+                    SlowYandexSearch::class.java,
+                    SlowBingSearch::class.java
+                )
+            )
+        ).apply {
             tell(SearchRequest("search"), ActorRef.noSender())
         }
     }
